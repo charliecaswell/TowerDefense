c46b3aa4c69fe1b4da7eb91a3506658070f33baa
charliecaswell <chcaswell@davidson.edu>
2020-07-19 19:34:25 -0400

    added more functionality to the enemies and towers

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    
+    [SerializeField] int hitPoints = 10;
+
+    void Start()
+    {
+        
+    } 
+
+    private void OnParticleCollision(GameObject other){
+        print("i am hit");
+        processHit();
+
+        //kill enemy 
+        if (hitPoints <= 0){
+            Destroy(gameObject);
+        }
+    }
+
+    void processHit(){
+        hitPoints -= 1;
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    
+    [SerializeField] Transform tower;
+    [SerializeField] Transform enemy; 
+    [SerializeField] float attackRange = 10f;
+    [SerializeField] ParticleSystem particle;
+    void Update(){
+        if (enemy){
+            tower.LookAt(enemy);
+            fireEnemy();
+        }
+        else{
+            var emmisionModule = particle.emission;
+            emmisionModule.enabled = false;
+        }
+    }
+
+    private void fireEnemy(){
+        float enemyDistance = Vector3.Distance(enemy.transform.position, gameObject.transform.position);
+        var emmisionModule = particle.emission;
+        //if within range fire
+        if (enemyDistance <= attackRange){
+            emmisionModule.enabled = true;
+        }
+        //otherwise turn particles off 
+        else{
+            emmisionModule.enabled = false;
+        }
+    }
+}
